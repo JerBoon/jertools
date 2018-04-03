c24e1088e62b238edb6e8850f7e30d76ca67efe0
Jer Boon <jer.booner@gmail.com>
2018-04-03 20:44:35 +0100

    comment

--- R/doc.R
+++ R/doc.R
@@ -1,3 +1,7 @@
+#calls the roxygen2 document() function by jumping in and out of the named directory
+#works for me with how I tend to develop these things..
+#YMMV
+
 doc <- function(path) {
   library(roxygen2)
   setwd(path)
